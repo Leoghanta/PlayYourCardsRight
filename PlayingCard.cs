76df10510b06e8853fd3f6c2eeee677c143293cf
Thomas Wilson <t.wilson@dundeeandangus.ac.uk>
2024-01-28 14:38:18 +0000

    Add project files.

---
+++ PlayingCard.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayYourCardsRight
+{
+    /// <summary>
+    /// ENUM of CardSuite
+    /// Hearts = 0, Diamonds = 1, Clubs = 2, Spades = 3
+    /// </summary>
+    public enum CardSuit
+    {
+        Hearts, 
+        Diamonds, 
+        Clubs,
+        Spades
+    }
+
+    /// <summary>
+    /// Playing Card Class holding a single card of a Rank and Suit
+    /// </summary>
+    internal class PlayingCard
+    {
+        public int Rank;
+        public CardSuit Suit;
+
+        /// <summary>
+        /// Constructor to create the card.
+        /// </summary>
+        /// <param name="rank">Number of card from 2 - 14</param>
+        /// <param name="suit">Card suit as an enum.</param>
+        public PlayingCard (int rank, CardSuit suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        /// <summary>
+        /// Override the ToString() so it prints the correct format of cards.
+        /// </summary>
+        /// <returns>current card as string.</returns>
+        public override string ToString()
+        {
+            string rankstring = "";
+
+            switch (Rank)
+            {
+                case 11:
+                    rankstring = "Jack";
+                    break;
+                case 12:
+                    rankstring = "Queen";
+                    break;
+                case 13:
+                    rankstring = "King";
+                    break;
+                case 14:
+                    rankstring = "Ace";
+                    break;
+                default:
+                    rankstring = Rank.ToString();
+                    break;
+            }
+            
+            return $"{rankstring} of {Suit}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayYourCardsRight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // New Game
+            Game game = new Game();
+            Console.WriteLine("Welcome to Play Your Cards Right.");
+
+            // Infinite Loop until broken.
+            while (true)
+            {
+                //Display the current card.
+                PlayingCard currentCard = game.getCurrentCard();
+                Console.WriteLine($"\nThe Current Card is {currentCard.ToString()}");
+
+                //Obtain user input.
+                Console.WriteLine("Is the next card Higher or Lower?");
+                Console.Write("Enter H/L/QUIT >> ");
+                string response = Console.ReadLine().ToLower();
+
+                ///Bye then!
+                if (response == "quit")
+                {
+                    Console.WriteLine("Thanks for playing");
+                    break;
+                }
+
+                // Response must be H or L. 
+                if (response == "h" || response == "l")
+                {
+                    /* response == h will give boolean True.
+                     * so using this in a parameter will send True value.
+                     * otherwise, False value will be sent 
+                     */
+                    if (game.Guess(response == "h")) 
+                    {
+                        Console.WriteLine($"Correct - Score: {game.GetScore()}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, you lose!");
+                        Console.WriteLine($"The next card was {game.getCurrentCard().ToString()}");
+                        Console.WriteLine($"Your final score was {game.GetScore()}");
+                        Console.WriteLine("\nThanks for playing!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Response!");
+                }
+                
+            }
+          
+            // Stop the console closing on end.
+            Console.ReadLine();
+        }
+    }
+}
